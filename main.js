f7cf32c95766744baeb7c0311709524ad8316b64
deepikaande <deepikaande363@gmail.com>
2024-04-03 23:46:18 +0530

    Porfolio changes

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed =new Typed(".text",{
+    strings: ["Frontend Developer","React Developer","Software Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+function toggleMenu() {
+  var navbar = document.getElementById('navbar');
+  navbar.classList.toggle('active');
+
+}
